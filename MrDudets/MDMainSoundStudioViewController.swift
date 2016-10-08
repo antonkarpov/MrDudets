b93b674d367490e7b8c9d6da0d5f2a16bad20b03
RomanPanichkin <rpanichkin@crystalnix.com>
2016-10-08 12:58:26 +0600

    Added main screen with collection view

---
+++ MrDudets/MDMainSoundStudioViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MDMainSoundStudioViewController.swift
+//  MrDudets
+//
+//  Created by RomanPanichkin on 08/10/16.
+//  Copyright © 2016 Anton Karpov. All rights reserved.
+//
+
+import UIKit
+
+class MDMainSoundStudioViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - Variables
+    
+    @IBOutlet var collectionView: UICollectionView!
+    var cellsDataSource: NSArray!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellsDataSource.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MusicalCell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
