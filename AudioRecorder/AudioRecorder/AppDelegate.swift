9ef5705552c686ad6088444190fedb5c8978928e
Anton <anton.karpov@crystalnix.com>
2016-10-09 12:21:24 +0600

    Add Test project for audio recording and playing

---
+++ AudioRecorder/AudioRecorder/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AudioRecorder
+//
+//  Created by Anton on 08/10/2016.
+//  Copyright © 2016 Anton Karpov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
